b0430d7f1c0bf3fcf46f06016d386233bdd90203
bladerunner2020 <pivovarov@gmail.com>
2017-12-03 17:39:31 +0300

    Release v1.1.0

---
+++ gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt){
+    grunt.config.init({
+        pkg: grunt.file.readJSON("package.json"),
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['-a'],
+                createTag: false,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false,
+                prereleaseName: false,
+                metadata: '',
+                regExp: false
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-bump');
+    grunt.registerTask('publish', ['bump:minor']);
+};
