01e56bb5e98e3d744b756a449ba9cdfdfd5e95ab
bladerunner2020 <pivovarov@gmail.com>
2018-01-25 20:43:50 +0300

    Added: Object.create changed: forEach 2nd argument - index

--- index.js
+++ index.js
@@ -21,6 +21,25 @@ if (IR) {
     };
 }
 
+if (typeof Object.create !== "function") {
+    Object.create = function (proto, propertiesObject) {
+        if (typeof proto !== 'object' && typeof proto !== 'function') {
+            throw new TypeError('Object prototype may only be an Object: ' + proto);
+        } else if (proto === null) {
+            throw new Error("This browser's implementation of Object.create is a shim and doesn't support 'null' as the first argument.");
+        }
+
+        if (typeof propertiesObject != 'undefined') {
+            throw new Error("This browser's implementation of Object.create is a shim and doesn't support a second argument.");
+        }
+
+        function F() {}
+        F.prototype = proto;
+
+        return new F();
+    };
+}
+
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString
 if (!Date.prototype.toISOString) {
     (function() {
@@ -179,7 +198,7 @@ if (!Array.prototype.forEach){
         var len = O.length;
 
         for (var i = 0; i < len; i++)
-            f(O[i]);
+            f(O[i], i);
     };
 }
 
